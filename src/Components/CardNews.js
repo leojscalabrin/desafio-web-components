5709861342dfc900e79c050a96ab957c02080c80
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-09-18 14:42:51 -0300

    setting classes

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -1,10 +1,28 @@
 class CardNews extends HTMLElement {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        const shadow = this.attachShadow({mode: "open"})
-        shadow.innerHTML = "<h1>Hello World</h1>";
-    }
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
+  }
+
+  build() {
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class", "card")
+
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card__left")
+    const cardRight = document.createElement("div");
+    cardLeft.setAttribute("class", "card__right")
+
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+
+    return componentRoot;
+  }
+
+  styles() {}
 }
 
-customElements.define('card-news', CardNews)
+customElements.define("card-news", CardNews);
