176e8b40b03848d37418fc909f6c93f8420cc9de
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-09-19 14:00:47 -0300

    props sub elementos

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -18,10 +18,11 @@ class CardNews extends HTMLElement {
     autor.textContent = "By" + (this.getAttribute("autor") || "Anonymous");
 
     const linkTitle = document.createElement("a");
-    linkTitle.textContent = this.getAttribute("title")
+    linkTitle.textContent = this.getAttribute("title");
+    linkTitle.href = this.getAttribute("link-url");
 
     const newsContent = document.createElement("p");
-    newsContent.textContent = this.getAttribute("content")
+    newsContent.textContent = this.getAttribute("content");
 
     cardLeft.appendChild(autor);
     cardLeft.appendChild(linkTitle);
@@ -29,7 +30,11 @@ class CardNews extends HTMLElement {
 
     const cardRight = document.createElement("div");
     cardRight.setAttribute("class", "card__right");
+
     const newsImage = document.createElement("img");
+    newsImage.src = this.getAttribute("photo");
+    newsImage.alt = "Foto da Notícia";
+
     cardRight.appendChild(newsImage);
 
     componentRoot.appendChild(cardLeft);
