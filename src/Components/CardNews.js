8cd138ef4626d7e6e27ea489d22ce2b7fc814021
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-09-19 13:49:13 -0300

    build component

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -9,12 +9,23 @@ class CardNews extends HTMLElement {
 
   build() {
     const componentRoot = document.createElement("div");
-    componentRoot.setAttribute("class", "card")
+    componentRoot.setAttribute("class", "card");
 
     const cardLeft = document.createElement("div");
-    cardLeft.setAttribute("class", "card__left")
+    cardLeft.setAttribute("class", "card__left");
+
+    const autor = document.createElement("span");
+    const linkTitle = document.createElement("a");
+    const newsContent = document.createElement("p");
+
+    cardLeft.appendChild(autor);
+    cardLeft.appendChild(linkTitle);
+    cardLeft.appendChild(newsContent);
+
     const cardRight = document.createElement("div");
-    cardLeft.setAttribute("class", "card__right")
+    cardRight.setAttribute("class", "card__right");
+    const newsImage = document.createElement("img")
+    cardRight.appendChild(newsImage)
 
     componentRoot.appendChild(cardLeft);
     componentRoot.appendChild(cardRight);
